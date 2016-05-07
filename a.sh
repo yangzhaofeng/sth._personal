9bdf60d1935869d45ce30e25418110fa5a671146
yangzhaofeng <yangzhaofengsteven@gmail.com>
2016-05-07 10:55:12 -0500

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,1 @@
+curl ftp://ftp.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add - echo "deb ftp://ftp.videolan.org/pub/debian/stable ./" | sudo tee /etc/apt/sources.list.d/libdvdcss.list sudo apt-get update sudo apt-get install libdvdcss2
